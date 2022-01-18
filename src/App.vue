a0c3af73e0ddfe5bd6023a9cef01d1b7c7a8bc8d
Tushxr00 <tushar.nandy@vidyamantra.com>
2022-01-18 12:51:28 +0530

    .
    .

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1> 
+    {{ title }}
+  </h1>
+  <p>welcome ....</p>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick" > Click me </button> -->
+
+  <div v-if="showModal">
+  <Modal :header="header" :text="text" theme='sale' @close="toggleModal"/>
+  </div>
+  <button @click="toggleModal">open modal</button>
+  </template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data () {
+    return{
+      title: `* My First Vue App *`,
+      header:"sign up for the give away",
+       text:" get ready",
+       showModal: false,
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.toggle(`active`)
+      this.$refs.name.focus()
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  },
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1{
+  text-transform: capitalize;
+  font-size: 5rem;
+}
+
+.modal.sale{
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1{
+  color: white;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="backdrop" @click="closeModal">
+        <div class="modal" :class="{sale : theme === 'sale'}">
+            <h1>
+                this is the Modal
+            </h1>
+            <p>
+                Modal content
+            </p>
+            <p>{{ header }}</p>
+            <p>{{ text  }}</p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header','text','theme'],
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: white;
+        border-radius: 10rem;
+    }
+
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1{
+        font-size: 2rem;
+        color: aqua;
+    }
+</style>
